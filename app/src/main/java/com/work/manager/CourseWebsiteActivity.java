b3b3ef03cb2713fe97c56944b2361e9b914973b2
hjiang00 <hjiang00@umail.ucsb.edu>
2017-12-13 22:54:17 -0800

    Adding course and new course website_v1

---
+++ app/src/main/java/com/work/manager/CourseWebsiteActivity.java
@@ -0,0 +1,13 @@
+package com.work.manager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CourseWebsiteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_website);
+    }
+}
